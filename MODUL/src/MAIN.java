79cbebe9376c4aa7528903e50e6095c37769c2f4
achmad habibi <habiby.ach@gmail.com>
2020-04-22 11:36:05 +0800

    membuat string array

--- MODUL/src/MAIN.java
+++ MODUL/src/MAIN.java
@@ -8,6 +8,10 @@ public class MAIN {
             char nilaiAbjad = 'B';
             boolean tampan = true;
             System.out.println(namaDepan + namaBelakang);
+
+        char [] uniskaChar = {'U','N','I','S','K','A'};
+        String uniskaString = new String(uniskaChar);
+            System.out.println(uniskaString);
         }
 
     }
