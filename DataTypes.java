c4b85b7abe0a2b100179ea464a4934263a0d3eaa
achmad habibi <habiby.ach@gmail.com>
2020-04-22 11:08:15 +0800

    Input dan scanner

--- DataTypes.java
+++ DataTypes.java
@@ -1,7 +1,10 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
+
 public class DataTypes{
  public static void main(String[] args){
-
+  Scanner scanner = new Scanner(System.in);
+  
   String namaDepan = "Achmad";
   String namaBelakang = "Habibi";
   int usia = 17;
@@ -9,6 +12,20 @@ public class DataTypes{
   double ipk = 3.142;
   char nilaiAbjad = 'B';
   boolean tampan = true;
+  
+  System.out.print("Input Nama Depan : ");
+  namaDepan = scanner.nextLine();
+  System.out.print("Input Nama Belakang : ");
+  namaBelakang = scanner.nextLine();
+  System.out.print("Input IPK : ");
+  ipk = scanner.nextDouble();
+  System.out.print("");
+  System.out.print("Input Nilai Abjad : ");
+  nilaiAbjad = scanner.next().charAt(0);
+  System.out.println("Tampan? :");
+  tampan = scanner.nextBoolean();
+
+  System.out.println("======OUTPUT=======");
   System.out.println("Nama depan : " + namaDepan);
   System.out.println("Nama Belakang : " + namaBelakang);
   System.out.println("Usia : " + usia);
