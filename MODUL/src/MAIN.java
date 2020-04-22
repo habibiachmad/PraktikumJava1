a808951346e1c6cf905cb285a0a42822a93e7e62
achmad habibi <habiby.ach@gmail.com>
2020-04-22 11:33:17 +0800

    PROJECT IDEA

---
+++ MODUL/src/MAIN.java
@@ -0,0 +1,15 @@
+public class MAIN {
+        public static void main(String[] args) {
+            String namaDepan = "Achmad";
+            String namaBelakang = "Habibi";
+            int usia = 17;
+            int targettahunkuliah = 3;
+            double ipk = 3.14285728;
+            char nilaiAbjad = 'B';
+            boolean tampan = true;
+            System.out.println(namaDepan + namaBelakang);
+        }
+
+    }
+
+}
