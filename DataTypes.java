d2a54449d53a12d3c148693e9b963e0804291f8c
achmad habibi <habiby.ach@gmail.com>
2020-04-22 10:14:40 +0800

    deklarasi variable dan tipe data

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes{
+ public static void(String[] args){
+
+  String namaDepan = "Achmad";
+  String namaTengah = "Habibi";
+  int usia = 17l
+  int targetTahunKuliah = 3;
+  double ipk = 3,142857143;
+  char nilai Abjad = 'B';
+  boolean tampan = true;
+
+}
+}
