16b3fb3a512e5aee751d42a07786e3ca77281b0d
achmad habibi <habiby.ach@gmail.com>
2020-04-22 11:42:26 +0800

    Operasi tipe string

--- MODUL/src/MAIN.java
+++ MODUL/src/MAIN.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class MAIN {
         public static void main(String[] args) {
             String namaDepan = "Achmad";
@@ -12,6 +14,20 @@ public class MAIN {
         char [] uniskaChar = {'U','N','I','S','K','A'};
         String uniskaString = new String(uniskaChar);
             System.out.println(uniskaString);
+
+        String namaLengkap = namaDepan + " " + namaBelakang;
+            System.out.println(namaLengkap.length());
+            System.out.println(namaLengkap.indexOf("NIS"));
+            System.out.println(namaLengkap.substring(5));
+            System.out.println(namaLengkap.substring(5,12));
+            System.out.println(namaLengkap.replace("yog",   "Barr"));
+            System.out.println(namaLengkap.toUpperCase());
+            System.out.println(namaLengkap.toLowerCase());
+            System.out.println(namaLengkap.charAt(6));
+            String [] namaArray = namaLengkap.split(" ");
+        for(String nama : namaArray){
+            System.out.println(nama);
+        }
         }
 
     }
